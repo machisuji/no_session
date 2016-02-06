18b32b6678ca5a9071c62bc96fccea1813eba3bc
Markus Kahl <machisuji@gmail.com>
2016-02-06 17:09:50 +0000

    there

---
+++ README.md
@@ -0,0 +1,3 @@
+# no_session
+
+Just a discourse plugin doing ... things.
---
+++ plugin.rb
@@ -0,0 +1,19 @@
+# name: no_session
+# about: it's a hack, really
+# version: 0.0.1
+# authors: Markus Kahl
+
+after_initialize do
+  module NOSession
+    def set_current_user_for_logs
+      super
+      cookies[:no_session] = {
+        value: "#{current_user.id}:#{current_user.no_session_salt}",
+        domain: String(ENV['DISCOURSE_HOSTNAME']).scan(/^.+?\.(.+)/).flatten.last
+        secure: true
+      }
+    end
+  end
+
+  ApplicationController.prepend NOSession
+end
